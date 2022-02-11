9848b074e4adec724bc2f67850edd666b525325f
Hamza Kermadi <hakermad@fu-r1-p7.s19.be>
2022-02-11 16:18:24 +0100

    add libft

---
+++ libft/ft_itoa.c
@@ -0,0 +1,59 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_itoa.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hakermad <hakermad@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/01/08 13:39:02 by hakermad          #+#    #+#             */
+/*   Updated: 2022/01/14 15:40:25 by hakermad         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+static int	ft_compteur(long nb)
+{
+	int	count;
+
+	count = 0;
+	if (nb < 0)
+	{
+		nb = nb * -1;
+		count++;
+	}
+	while (nb > 0)
+	{
+		nb = nb / 10;
+		count++;
+	}
+	return (count);
+}
+
+char	*ft_itoa(int n)
+{
+	char	*str;
+	long	nb;
+	int		i;
+
+	if (!n)
+		return (ft_strdup("0"));
+	nb = n;
+	i = ft_compteur(nb);
+	str = malloc(sizeof(*str) * (i + 1));
+	if (!str)
+		return (NULL);
+	str[i--] = '\0';
+	if (nb < 0)
+	{
+		str[0] = '-';
+		nb = nb * -1;
+	}
+	while (nb > 0)
+	{
+		str[i] = 48 + (nb % 10);
+		nb = nb / 10;
+		i--;
+	}
+	return (str);
+}
---
+++ libft/ft_lstnew.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstnew.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hakermad <hakermad@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/01/11 13:20:27 by hakermad          #+#    #+#             */
+/*   Updated: 2022/01/12 14:36:28 by hakermad         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+t_list	*ft_lstnew(void *content)
+{
+	t_list	*new_element;
+
+	new_element = (t_list *)malloc(sizeof(t_list));
+	if (!new_element)
+		return (NULL);
+	new_element->content = content;
+	new_element->next = NULL;
+	return (new_element);
+}
