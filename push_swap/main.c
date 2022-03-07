e596311e445e6477a0f65e04f7ab55590333a620
Hamza <hamza@MacBook-Air-de-Marine.local>
2022-03-07 17:48:43 +0100

    fix error case_five

--- push_swap/main.c
+++ push_swap/main.c
@@ -24,36 +24,9 @@ int main(int argc, char **argv)
         case_three(&stack_a);
     //if(argc - 1 == 4 || argc - 1 == 5)
         //case_four(&stack_a,&stack_b);
-    if(argc - 1 == 4 || argc - 1 == 5)
+    if(argc - 1 == 5)
         case_five(&stack_a,&stack_b);
      if(argc - 1 > 5)
         radix_sort(&stack_a,&stack_b);
-
-
-    //case_three(&stack_a);
-    //case_four(&stack_a, &stack_b);
-    //radix_sort(&stack_a, &stack_b);
-    //case_five(&stack_a, &stack_b);
     //ft_printlst(stack_a);
 }
-
-// int	main(int argc, char **argv)
-// {
-// 	t_list	**stack_a;
-// 	t_list	**stack_b;
-
-// 	if (argc < 2)
-// 		return (-1);
-// 	ft_pars(argc, argv);
-// 	stack_a = (t_list **)malloc(sizeof(t_list));
-// 	stack_b = (t_list **)malloc(sizeof(t_list));
-// 	*stack_a = NULL;
-// 	*stack_b = NULL;
-// 	initStack(stack_a, argc, argv);
-// 	if (is_sorted(stack_a))
-// 	{
-// 		return (0);
-// 	}
-// 	sort_stack(stack_a, stack_b);
-// 	return (0);
-// }
